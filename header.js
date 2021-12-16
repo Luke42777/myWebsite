7ac71c846c4e2c47e1d58cd6df393c605d26f435
Luke42777 <55854062+Luke42777@users.noreply.github.com>
2021-12-16 12:38:15 +0000

    menu hamburger done, still opacity and blur on header needs to be done when menu is slided

---
+++ header.js
@@ -0,0 +1,15 @@
+const bars = document.querySelector(".fa-bars");
+const times = document.querySelector(".fa-times");
+const burger = document.querySelector(".burger");
+const menu = document.querySelector("aside.menu");
+
+    
+burger.addEventListener("click",() => {
+bars.classList.toggle("show");
+times.classList.toggle("show");
+menu.classList.toggle("active");
+});
+
+
+
+
