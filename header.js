4b4210b07d043d9e68442ced13b97ba1744a8b98
Luke42777 <55854062+Luke42777@users.noreply.github.com>
2021-12-18 18:15:08 +0000

    animation added to images and welcome banner, menu buttons(showing line animation done after hover)

--- header.js
+++ header.js
@@ -3,11 +3,11 @@ const times = document.querySelector(".fa-times");
 const burger = document.querySelector(".burger");
 const menu = document.querySelector("aside.menu");
 
-    
-burger.addEventListener("click",() => {
-bars.classList.toggle("show");
-times.classList.toggle("show");
-menu.classList.toggle("active");
+
+burger.addEventListener("click", () => {
+    bars.classList.toggle("show");
+    times.classList.toggle("show");
+    menu.classList.toggle("active");
 });
 
 
