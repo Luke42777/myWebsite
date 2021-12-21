1eb79f5cf4377be8e0fdbd0edf1d820ce3378190
Luke42777 <55854062+Luke42777@users.noreply.github.com>
2021-12-21 22:12:18 +0000

    photos of myself added, section portfolio added but pressing button in nav doesnt lead to this section

--- nav.js
+++ nav.js
@@ -1,16 +1,26 @@
 const buttons = document.querySelectorAll(".menu nav ul li a");
 const technoPos = document.querySelector('.techno').offsetTop;
+const portfolioPos = document.querySelector('.portfolio').offsetTop;
+console.log(portfolioPos);
 
 
 
 const scrollToSection = function (event) {
     event.preventDefault();
     menu.classList.toggle("active");
+    bars.classList.toggle("show");
+    times.classList.toggle("show");
     if (this.className === "tech") {
         window.scrollTo({
             top: technoPos,
             behavior: 'smooth',
-          }); 
+        });
+    }
+    else if (this.className === "portf") {
+        window.scrollTo({
+            top: 1766,
+            behavior: 'smooth',
+        });
     }
 }
 
