5727bd4b9387d6bffdb5ffa36111d524683bf268
Luke42777 <55854062+Luke42777@users.noreply.github.com>
2021-12-29 18:10:04 +0000

    section hobby done(sliding, responsivity)

---
+++ hobby.js
@@ -0,0 +1,20 @@
+
+$(document).on("scroll", () => {
+    const $scrollValue = $(this).scrollTop();
+    const $spotToAnimate1 = $("section.hobby").offset().top - 40;
+    const $spotToAnimate2 = $("section.hobby div.eat").offset().top;
+    const $spotToAnimate3 = $("section.hobby div.climb").offset().top;
+
+    if ($scrollValue >= $spotToAnimate1) {
+        $("section.hobby div.climb").addClass("active");
+        $("section.hobby div.eat").addClass("active");
+    }
+    if ($scrollValue > $spotToAnimate2) {
+        $("section.hobby div.walk").addClass("active");
+    }
+    if ($scrollValue > $spotToAnimate3) {
+        $("section.hobby div.paint").addClass("active");
+    }
+
+})
+
