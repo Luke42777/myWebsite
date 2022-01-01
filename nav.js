22670b15ef2b711b70ca511cb9480aa3c6029f66
Luke42777 <55854062+Luke42777@users.noreply.github.com>
2022-01-01 14:53:50 +0000

    fromsubmit add2

--- nav.js
+++ nav.js
@@ -3,15 +3,16 @@ const $menu = $(".menu");
 const $bars = $(".fa-bars");
 const $times = $(".fa-times");
 
-$buttons.on("click", function(event){
+$buttons.on("click", function (event) {
     event.preventDefault();
+    this.classList.add("active");
     $menu.toggleClass("active");
     $bars.toggleClass("show");
     $times.toggleClass("show");
-    if(this.hash !== ""){
+    if (this.hash !== "") {
         const hash = this.hash;
         $("html,body").animate({
             scrollTop: $(hash).offset().top
-        },800)
+        }, 800)
     }
 })
